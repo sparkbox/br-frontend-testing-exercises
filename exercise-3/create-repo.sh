d9ca7a3b9b675cbcb4d30a3d36e18bd9c999c1ac
Ryan Cromwell <ryan@heysparkbox.com>
2015-02-10 12:06:05 -0500

    feat (cli): remove scripted setup instructions

--- exercise-3/create-repo.sh
+++
@@ -1,7 +0,0 @@
-cp -R br-frontend-testing-flow ../.. && \
-  cd ../../br-frontend-testing-flow && \
-  git init && \
-  git add . && \
-  git commit -m "init repo" && \
-  echo "Your project is at $(pwd)"
-

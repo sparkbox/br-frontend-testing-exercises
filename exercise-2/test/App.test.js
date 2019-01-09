8e354c03aae80223649e30e9b7719cacaf5f80c1
Jon Oliver <jon@heysparkbox.com>
2019-01-09 14:05:31 -0500

    test (exercise-2): Add pending UI test for App

--- exercise-2/test/App.test.js
+++ exercise-2/test/App.test.js
@@ -13,6 +13,33 @@ describe('App', () => {
 
     new App().start();
 
-    expect(document.querySelector('#classification').innerHTML).toEqual('');
+    const classification = document.querySelector('#classification');
+    expect(classification.innerHTML).toEqual('');
+  });
+
+  xit('updates the classification on submit', () => {
+    render`
+      <form>
+        <input type="text" id="sidea">
+        <input type="text" id="sideb">
+        <input type="text" id="sidec">
+      </form>
+      <div id="classification"></div>
+    `;
+
+    new App().start();
+
+    // fill in the form
+    const form = document.querySelector('form');
+    form.elements.sidea.value = 1;
+    form.elements.sideb.value = 1;
+    form.elements.sidec.value = 1;
+
+    // submit the form
+    const event = new Event('submit');
+    form.dispatchEvent(event);
+
+    const classification = document.querySelector('#classification');
+    expect(classification.innerHTML).toEqual('equilateral');
   });
 });
